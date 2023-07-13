58fc13f0ba197ed9d118d580bf6db77e125104d6
Liusy Divakov <divakovwork@gmail.com>
2023-07-13 23:49:46 +0300

    update 9 files and delete 1 file

--- mebel-lux/pages/Home.jsx
+++ mebel-lux/pages/Home.jsx
@@ -6,29 +6,8 @@ import Footer from '/components/footer.jsx'
 
 
 
+
 function Home() {
-  useEffect(() => {
-    const open = document.getElementById("menu-btn");
-    const nav = document.getElementById("nav");
-    const exit = document.getElementById("exit-btn");
-
-    const handleOpenClick = () => {
-      nav.classList.add("open-nav");
-    };
-
-    const handleExitClick = () => {
-      nav.classList.remove("open-nav");
-    };
-
-    open.addEventListener("click", handleOpenClick);
-    exit.addEventListener("click", handleExitClick);
-
-    return () => {
-      open.removeEventListener("click", handleOpenClick);
-      exit.removeEventListener("click", handleExitClick);
-    };
-  }, []);
-
   useEffect(() => {
     const handleResize = () => {
       const widthElement = document.getElementById("width");
@@ -52,7 +31,6 @@ function Home() {
       <Header />
       <Main />
       <Footer/>
-
     </>
   );
 }
