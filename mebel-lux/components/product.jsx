31ea2a3b89408ad9dde3226dc22cad88ac5ff7d7
Liusy Divakov <divakovwork@gmail.com>
2023-07-19 22:06:41 +0300

    delete 1 file and update 3 files

---
+++ mebel-lux/components/product.jsx
@@ -0,0 +1,10 @@
+export default function Product(){
+    return(
+        <>
+            <div className="prod">
+                <img className="prod-img" src="../images/background1.png" alt="product image" />
+                <h4 className="prod-title">Product Title</h4>            
+            </div>
+        </>
+    )
+}
