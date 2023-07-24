f1f2c18bc108ef043b00e451ed9e54187b87d313
Liusy Divakov <divakovwork@gmail.com>
2023-07-24 14:41:38 +0300

    update 6 files

--- mebel-lux/components/contactsMain.jsx
+++ mebel-lux/components/contactsMain.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
+import emailjs from "emailjs-com"
 
 export default function Main() {
+function sendEmail(){
+}
+
   return (
     <>
       <main id="contacts-main">
@@ -8,13 +12,13 @@ export default function Main() {
         <h4 className="phone-title"><span>Телефон:</span> 0898688821</h4>
         <h4 className="email-title"><span>Email:</span> mebellux@abv.bg</h4>
         <h4 className="addres-title">Адрес: <span className="addres-span">гр. Велинград, ул. "Чавдар Войвода" 29, 4600</span></h4>
-        <div className="inputs-container">
-            <input type="text" placeholder="Име"/>
-            <input type="text" placeholder="Email"/>
-            <input type="text" placeholder="Телефон"/>
-            <textarea type="text" placeholder="Съобщение"/>
-        </div>
-        <button className="submit-btn" type="submit">Изпрати</button>
+        <form className="inputs-container">
+            <input name="name" type="text" placeholder="Име" required/>
+            <input name="email" type="text" placeholder="Email" required/>
+            <input name="phone" type="text" placeholder="Телефон" required/>
+            <textarea name="message" type="text" placeholder="Съобщение"/>
+            <button className="submit-btn" type="submit" onSubmit={sendEmail}>Изпрати</button>
+        </form>
       </main>
     </>
   );
