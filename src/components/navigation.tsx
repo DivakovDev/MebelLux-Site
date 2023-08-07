85dac83be367b0c6fef17b1b253f16bc7c3aee1f
Liusy Divakov <divakovwork@gmail.com>
2023-08-07 12:31:05 +0300

    delete 1 file and update 1 file

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,6 +1,6 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
 import {
   BookmarkSquareIcon,
   CalendarIcon,
@@ -14,69 +14,92 @@ import {
   LifebuoyIcon,
   Squares2X2Icon,
   XMarkIcon,
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon } from '@heroicons/react/24/solid'
+} from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
 
 const solutions = [
   {
-    name: 'Analytics',
-    description: 'Get a better understanding of where your traffic is coming from.',
-    href: '#',
-    icon: ChartBarIcon,
+    name: "Спални",
+    description: "Създайте уют и комфорт във вашата спалня",
+    href: "#",
+    icon: "ArrowPathIcon",
   },
   {
-    name: 'Engagement',
-    description: 'Speak directly to your customers in a more meaningful way.',
-    href: '#',
-    icon: CursorArrowRaysIcon,
-  },
-  { name: 'Security', description: "Your customers' data will be safe and secure.", href: '#', icon: ShieldCheckIcon },
-  {
-    name: 'Integrations',
-    description: "Connect with third-party tools that you're already using.",
-    href: '#',
-    icon: Squares2X2Icon,
+    name: "Антрета",
+    description: "Структурирайте входното пространство на вашия дом",
+    href: "#",
+    icon: "ArrowPathIcon",
   },
   {
-    name: 'Automations',
-    description: 'Build strategic funnels that will drive your customers to convert',
-    href: '#',
-    icon: ArrowPathIcon,
-  },
-]
-const callsToAction = [
-  { name: 'Watch Demo', href: '#', icon: PlayIcon },
-  { name: 'Contact Sales', href: '#', icon: PhoneIcon },
-]
-const resources = [
-  {
-    name: 'Help Center',
-    description: 'Get all of your questions answered in our forums or contact support.',
-    href: '#',
-    icon: LifebuoyIcon,
+    name: "Кухни",
+    description: "Модерни и функционални кухни за готвене и хранене",
+    href: "#",
+    icon: "ArrowPathIcon",
   },
   {
-    name: 'Guides',
-    description: 'Learn how to maximize our platform to get the most out of it.',
-    href: '#',
-    icon: BookmarkSquareIcon,
+    name: "Детски-легла",
+    description: "Зарадвайте вашето дете с удобни и забавни легла",
+    href: "#",
+    icon: "ArrowPathIcon",
   },
   {
-    name: 'Events',
-    description: 'See what meet-ups and other events we might be planning near you.',
-    href: '#',
-    icon: CalendarIcon,
+    name: "Бюра",
+    description: "Организирайте си работното пространство у дома",
+    href: "#",
+    icon: "ArrowPathIcon",
   },
-  { name: 'Security', description: 'Understand how we take your privacy seriously.', href: '#', icon: ShieldCheckIcon },
-]
-const recentPosts = [
-  { id: 1, name: 'Boost your conversion rate', href: '#' },
-  { id: 2, name: 'How to use search engine optimization to drive traffic to your site', href: '#' },
-  { id: 3, name: 'Improve your customer experience', href: '#' },
-]
+  {
+    name: "Нощни-Шкафчета",
+    description: "Добавете стил и удобство до вашия спален интериор",
+    href: "#",
+    icon: "ArrowPathIcon",
+  },
+  {
+    name: "Секции",
+    description: "Модулни системи за съхранение и организация",
+    href: "#",
+    icon: "ArrowPathIcon",
+  },
+  {
+    name: "Гардероби",
+    description: "Просторни и функционални гардероби за вашия дом",
+    href: "#",
+    icon: "ArrowPathIcon",
+  },
+  {
+    name: "Скринове",
+    description: "Елегантни и практични скринове за всяка стая",
+    href: "#",
+    icon: "ArrowPathIcon",
+  },
+  {
+    name: "Маси",
+    description: "Разнообразие от маси за вашата дневна или трапезария",
+    href: "#",
+    icon: "ArrowPathIcon",
+  },
+  {
+    name: "Тоалетки",
+    description: "Изискани и функционални тоалетки за грим и грижа за себе си",
+    href: "#",
+    icon: "ArrowPathIcon",
+  },
+  {
+    name: "ТВ-барчета",
+    description: "Стилни решения за подредено ТВ пространство",
+    href: "#",
+    icon: "ArrowPathIcon",
+  },
+  {
+    name: "Градински-мебели",
+    description: "Удобство и естетика за вашата градина или тераса",
+    href: "#",
+    icon: "ArrowPathIcon",
+  },
+];
 
 function classNames(...classes: any[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function NavigationBar() {
@@ -85,11 +108,11 @@ export default function NavigationBar() {
       <div className="px-4 mx-auto max-w-7xl sm:px-6">
         <div className="flex items-center justify-between py-6 border-b-2 border-gray-100 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
-              <span className="sr-only">Workflow</span>
+            <a href="/">
+              <span className="w-12 h-12 sr-only">Workflow</span>
               <img
                 className="w-auto h-8 sm:h-10"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                src="../images/mebel-lux-phoenix-logo.png"
                 alt=""
               />
             </a>
@@ -106,15 +129,15 @@ export default function NavigationBar() {
                 <>
                   <Popover.Button
                     className={classNames(
-                      open ? 'text-gray-900' : 'text-gray-500',
-                      'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                      open ? "text-gray-900" : "text-gray-500",
+                      "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     )}
                   >
-                    <span>Solutions</span>
+                    <span>Продукти</span>
                     <ChevronDownIcon
                       className={classNames(
-                        open ? 'text-gray-600' : 'text-gray-400',
-                        'ml-2 h-5 w-5 group-hover:text-gray-500'
+                        open ? "text-gray-600" : "text-gray-400",
+                        "ml-2 h-5 w-5 group-hover:text-gray-500"
                       )}
                       aria-hidden="true"
                     />
@@ -138,25 +161,15 @@ export default function NavigationBar() {
                               href={item.href}
                               className="flex items-start p-3 -m-3 rounded-lg hover:bg-gray-50"
                             >
-                              <item.icon className="flex-shrink-0 w-6 h-6 text-indigo-600" aria-hidden="true" />
                               <div className="ml-4">
-                                <p className="text-base font-medium text-gray-900">{item.name}</p>
-                                <p className="mt-1 text-sm text-gray-500">{item.description}</p>
+                                <p className="text-base font-medium text-gray-900">
+                                  {item.name}
+                                </p>
+                                <p className="mt-1 text-sm text-gray-500">
+                                  {item.description}
+                                </p>
                               </div>
                             </a>
-                          ))}
-                        </div>
-                        <div className="px-5 py-5 space-y-6 bg-gray-50 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                          {callsToAction.map((item) => (
-                            <div key={item.name} className="flow-root">
-                              <a
-                                href={item.href}
-                                className="flex items-center p-3 -m-3 text-base font-medium text-gray-900 rounded-md hover:bg-gray-100"
-                              >
-                                <item.icon className="flex-shrink-0 w-6 h-6 text-gray-400" aria-hidden="true" />
-                                <span className="ml-3">{item.name}</span>
-                              </a>
-                            </div>
                           ))}
                         </div>
                       </div>
@@ -166,94 +179,25 @@ export default function NavigationBar() {
               )}
             </Popover>
 
-            <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-              Pricing
-            </a>
-            <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-              Docs
-            </a>
-
-            <Popover className="relative">
-              {({ open }) => (
-                <>
-                  <Popover.Button
-                    className={classNames(
-                      open ? 'text-gray-900' : 'text-gray-500',
-                      'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                    )}
-                  >
-                    <span>More</span>
-                    <ChevronDownIcon
-                      className={classNames(
-                        open ? 'text-gray-600' : 'text-gray-400',
-                        'ml-2 h-5 w-5 group-hover:text-gray-500'
-                      )}
-                      aria-hidden="true"
-                    />
-                  </Popover.Button>
-
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="opacity-0 translate-y-1"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in duration-150"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 translate-y-1"
-                  >
-                    <Popover.Panel className="absolute z-10 w-screen max-w-md px-2 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0">
-                      <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                        <div className="relative grid gap-6 px-5 py-6 bg-white sm:gap-8 sm:p-8">
-                          {resources.map((item) => (
-                            <a
-                              key={item.name}
-                              href={item.href}
-                              className="flex items-start p-3 -m-3 rounded-lg hover:bg-gray-50"
-                            >
-                              <item.icon className="flex-shrink-0 w-6 h-6 text-indigo-600" aria-hidden="true" />
-                              <div className="ml-4">
-                                <p className="text-base font-medium text-gray-900">{item.name}</p>
-                                <p className="mt-1 text-sm text-gray-500">{item.description}</p>
-                              </div>
-                            </a>
-                          ))}
-                        </div>
-                        <div className="px-5 py-5 bg-gray-50 sm:px-8 sm:py-8">
-                          <div>
-                            <h3 className="text-sm font-medium tracking-wide text-gray-500 uppercase">Recent Posts</h3>
-                            <ul role="list" className="mt-4 space-y-4">
-                              {recentPosts.map((post) => (
-                                <li key={post.id} className="text-base truncate">
-                                  <a href={post.href} className="font-medium text-gray-900 hover:text-gray-700">
-                                    {post.name}
-                                  </a>
-                                </li>
-                              ))}
-                            </ul>
-                          </div>
-                          <div className="mt-5 text-sm">
-                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                              {' '}
-                              View all posts <span aria-hidden="true">&rarr;</span>
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-                    </Popover.Panel>
-                  </Transition>
-                </>
-              )}
-            </Popover>
-          </Popover.Group>
-          <div className="items-center justify-end hidden md:flex md:flex-1 lg:w-0">
-            <a href="#" className="text-base font-medium text-gray-500 whitespace-nowrap hover:text-gray-900">
-              Sign in
-            </a>
             <a
               href="#"
+              className="text-base font-medium text-gray-500 hover:text-gray-900"
+            >
+              Партньори
+            </a>
+            <a
+              href="/about"
+              className="text-base font-medium text-gray-500 hover:text-gray-900"
+            >
+              За нас
+            </a>
+          </Popover.Group>
+          <div className="items-center justify-end hidden md:flex md:flex-1 lg:w-0">
+            <a
+              href="/contacts"
               className="inline-flex items-center justify-center px-4 py-2 ml-8 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm whitespace-nowrap hover:bg-indigo-700"
             >
-              Sign up
+              Контакти
             </a>
           </div>
         </div>
@@ -268,7 +212,10 @@ export default function NavigationBar() {
         leaveFrom="opacity-100 scale-100"
         leaveTo="opacity-0 scale-95"
       >
-        <Popover.Panel focus className="absolute inset-x-0 top-0 p-2 transition origin-top-right transform md:hidden">
+        <Popover.Panel
+          focus
+          className="absolute inset-x-0 top-0 p-2 transition origin-top-right transform md:hidden"
+        >
           <div className="bg-white divide-y-2 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 divide-gray-50">
             <div className="px-5 pt-5 pb-6">
               <div className="flex items-center justify-between">
@@ -294,50 +241,17 @@ export default function NavigationBar() {
                       href={item.href}
                       className="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50"
                     >
-                      <item.icon className="flex-shrink-0 w-6 h-6 text-indigo-600" aria-hidden="true" />
-                      <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
+                      <span className="ml-3 text-base font-medium text-gray-900">
+                        {item.name}
+                      </span>
                     </a>
                   ))}
                 </nav>
-              </div>
-            </div>
-            <div className="px-5 py-6 space-y-6">
-              <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
-                  Pricing
-                </a>
-
-                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
-                  Docs
-                </a>
-                {resources.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-              <div>
-                <a
-                  href="#"
-                  className="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700"
-                >
-                  Sign up
-                </a>
-                <p className="mt-6 text-base font-medium text-center text-gray-500">
-                  Existing customer?{' '}
-                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
-                    Sign in
-                  </a>
-                </p>
               </div>
             </div>
           </div>
         </Popover.Panel>
       </Transition>
     </Popover>
-  )
+  );
 }
