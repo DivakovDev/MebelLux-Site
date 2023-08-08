c8ca4055145e907dcd49a0007856afebb647c72a
Liusy Divakov <divakovwork@gmail.com>
2023-08-08 22:43:46 +0300

    update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,4 +1,5 @@
 import Product from "./product"
+import React, { useState } from "react";
 // add those items ito the products array:
 
 const products = [
@@ -50,29 +51,96 @@ const products = [
         imageSrc: '/images/antre.jpg',
         href: '/products/antres',
     },
+    {
+        id: 7,
+        name: 'Детски легла',
+        description: 'Детски легла с различни дизайни и цветове',
+        options: 'Размери: 80/160, 90/190, 120/200',
+        imageSrc: '/images/children-beds.jpg',
+        href: '/products/children-beds',
+    },
+    {
+        id: 8,
+        name: 'Секции',
+        description: 'Секции за всекидневна или хол с различни стилове',
+        options: 'Размери: разнообразни',
+        imageSrc: '/images/sections.jpg',
+        href: '/products/sections',
+    },
+    {
+        id: 9,
+        name: 'Скринове',
+        description: 'Скринове с различни размери и дизайни',
+        options: 'Размери: разнообразни',
+        imageSrc: '/images/dressers.jpg',
+        href: '/products/dressers',
+    },
+    {
+        id: 10,
+        name: 'Маси',
+        description: 'Маси за хранене или кафе с различни стилове',
+        options: 'Размери: разнообразни',
+        imageSrc: '/images/tables.jpg',
+        href: '/products/tables',
+    },
+    {
+        id: 11,
+        name: 'Тоалетки',
+        description: 'Тоалетки с огледало и различни стилове',
+        options: 'Размери: разнообразни',
+        imageSrc: '/images/vanities.jpg',
+        href: '/products/vanities',
+    },
+    {
+        id: 12,
+        name: 'ТВ барчета',
+        description: 'ТВ барчета с различни конфигурации и цветове',
+        options: 'Размери: разнообразни',
+        imageSrc: '/images/tv-stands.jpg',
+        href: '/products/tv-stands',
+    },
+    {
+        id: 13,
+        name: 'Градински мебели',
+        description: 'Мебели за градината с различни дизайни',
+        options: 'Размери: разнообразни',
+        imageSrc: '/images/outdoor-furniture.jpg',
+        href: '/products/outdoor-furniture',
+    },
   ]
 
 export default function Products() {
+const [showAllProducts, setShowAllProducts] = React.useState(false)
+
     return (
         <div className="bg-white">
-                <h2 className="text-black text-4xl text-center pt-12 font-bold">Products</h2>
-            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
-
-                <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
-                    {products.map((product) => (
-                        <Product
-                            key={product.id}
-                            id={product.id}
-                            img={product.imageSrc}
-                            title={product.name}
-                            description={product.description}
-                            options={product.options}
-                            href={product.href}
-                        />
-                    ))}
-                </div>
+        <h2 className="pt-12 text-4xl font-bold text-center text-black">Продукти</h2>
+        <div className="max-w-2xl px-4 py-16 mx-auto sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+                {products.slice(0, showAllProducts ? products.length : 6).map((product) => (
+                    <Product
+                        key={product.id}
+                        id={product.id}
+                        img={product.imageSrc}
+                        title={product.name}
+                        description={product.description}
+                        options={product.options}
+                        href={product.href}
+                    />
+                ))}
             </div>
+            {!showAllProducts && (
+                <div className="flex justify-center mt-4">
+                    <button
+                        onClick={() => setShowAllProducts(true)}
+                        className="px-4 py-2 font-medium text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    >
+                        View More
+                    </button>
+                </div>
+            )}
         </div>
+    </div>
     )
 }
   
