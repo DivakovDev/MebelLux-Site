8bcc4bad5776b74e1a32e78671250e2b92a38c8c
Liusy Divakov <divakovwork@gmail.com>
2023-08-08 23:01:13 +0300

    delete 3 files and update 1 file

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -23,13 +23,15 @@ function Products() {
 
   return (
     <section className="p-8">
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
         <Product img="/images/background1.png" title="Спални" id={""} description={""} options={""} href={""} />
         <Product img="/images/kitchen.jpg" title="Кухни" id={""} description={""} options={""} href={""} />
         <Product img="/images/weardrop.jpg" title="Гардероби" id={""} description={""} options={""} href={""} />
         <Product img="/images/bedtables.jpg" title="Нощни шкафчета" id={""} description={""} options={""} href={""} />
         <Product img="/images/desks.jpg" title="Бюра" id={""} description={""} options={""} href={""} />
         <Product img="/images/antre.jpg" title="Антрета" id={""} description={""} options={""} href={""} />
+        <Product img="/images/children-beds.jpg" title="Детски легла" id={""} description={""} options={""} href={""} />
+        
       </div>
     </section>
   );
